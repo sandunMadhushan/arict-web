da485f7c534d93d82f58e562a0a219b3a17539b1
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2026-04-24 08:25:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/arict-web/",
+  base: process.env.VERCEL ? "/" : "/arict-web/",
 })
